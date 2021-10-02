7a3e87d0d6c5de2063076c5366bce2d380e21d7a
AndaChain <andaanda789@gmail.com>
2021-10-03 00:32:28 +0700

    fix for auth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
+    provider :google_oauth2, "221332439850-fj84htsk9ika4vlmalshagabet9vg4v9.apps.googleusercontent.com","5e0LG3n2-ZL_y8EcaEXuC8IN"
   end
