18e37ea9f47581b1bc7b305f0ca496acc69a4021
panat54083 <panat54083@gmail.com>
2021-10-03 07:07:20 -0700

    movie_spec_Pass

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -29,15 +29,17 @@ class Movie < ActiveRecord::Base
     }
     class Movie::InvalidKeyError < StandardError ; end
 
-	def self.find_in_tmdb(string)
-		begin
-			Tmdb::Movie.find(string)
-		rescue Tmdb::InvalidApiKeyError
-			raise Movie::InvalidKeyError, 'Invalid API key'
-		end
-	end
-    	def name_with_rating
+    def self.find_in_tmdb(string)
+      begin
+        Tmdb::Movie.find(string)
+      rescue Tmdb::InvalidApiKeyError
+        raise Movie::InvalidKeyError, 'Invalid API key'
+      end
+    end
+
+    def name_with_rating
 		"#{self.title} (#{self.rating})"
 	end
+    
 end
 
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,12 +1,20 @@
+require 'rails_helper'
 
-require 'rails_helper.rb'
-
-# in spec/models/movie_spec.rb
-  describe Movie do
-    describe 'searching Tmdb by keyword' do
-      it 'calls Tmdb with title keywords' do
-        expect(Tmdb::Movie).to receive(:find).with('Inception')
-        Movie.find_in_tmdb('Inception')
+describe Movie do
+  describe 'searching Tmdb by keyword' do
+    context 'with valid API key' do
+      # elided for brevity
+    end
+    context 'with invalid API key' do
+      before(:each) do
+        allow(Tmdb::Movie).to receive(:find).and_raise(Tmdb::InvalidApiKeyError)
       end
+      # all specs in this block can take advantage of line 10's stub
+      it 'raises an InvalidKeyError' do
+        expect { Movie.find_in_tmdb('Inception') }.
+          to raise_error(Movie::InvalidKeyError)
+      end
+      # other sad-path specs here...
     end
   end
+end
