13d5ef10315ebadd5483446f6cfb53d44ee28752
panat54083 <panat54083@gmail.com>
2021-10-03 07:00:53 -0700

    Other

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,11 +2,11 @@
 require 'rails_helper.rb'
 
 # in spec/models/movie_spec.rb
-describe Movie do
-    it 'should include rating and year in full name' do
-      # 'build' creates but doesn't save object; 'create' also saves it
-      movie = FactoryGirl.build(:movie, :title => 'Milk', :rating => 'R')
-      expect(movie.name_with_rating).to eq 'Milk (R)'
+  describe Movie do
+    describe 'searching Tmdb by keyword' do
+      it 'calls Tmdb with title keywords' do
+        expect(Tmdb::Movie).to receive(:find).with('Inception')
+        Movie.find_in_tmdb('Inception')
+      end
     end
   end
-  
